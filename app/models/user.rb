db4bf733c9807e2bd6975aa4ea90a3b717731217
allenlr <allenlru55@gmail.com>
2023-09-07 21:58:01 -0500

    correct if statement in user validation to use lambda, add review validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, :email, uniqueness: true, presence: true
-    validates :password, length: { minimum: 6}, if -> { new_record? || !password.nil?}
+    validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil?}
     
     has_many :reviews
     has_many :albums, through: :reviews
