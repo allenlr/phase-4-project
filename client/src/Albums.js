a8284632611561eeaa4835840df37a3aa35f4b2d
allenlr <allenlru55@gmail.com>
2023-09-11 00:13:52 -0500

    change switch to routes, and component attribute to element in App component

---
+++ client/src/Albums.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Albums(){
+    return(
+        <div>
+            Albums
+        </div>
+    )
+}
+
+
+export default Albums;
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
+import Home from './Home';
+import Albums from './Albums';
+import Login from './Login';
+import Register from './Register';
+import NotFound from './NotFound';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/albums" component={Albums} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
