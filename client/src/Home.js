6b0273aaf8ee1b16c5cce66c35a509fa25122176
allenlr <allenlru55@gmail.com>
2023-09-22 21:26:30 -0500

    clean up unused useEffect and login component in Home component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,18 +1,20 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import UserContext from './context/UserContext';
-import Login from './Login'
 
 
 function Home(){
     const { currentUser, isLoading } = useContext(UserContext)
+
+    console.log(currentUser)
     
     if(isLoading) return <div>Loading...</div>
-    // if(!currentUser) return <Login />
-    return(
-        <div>
-            Welcome {currentUser?.username}
-        </div>
-    )
+    else{
+        return(
+            <div>
+                Welcome {currentUser?.username}
+            </div>
+        )
+    }
 }
 
 export default Home;
