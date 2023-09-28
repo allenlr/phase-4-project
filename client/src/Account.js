fd57532b030268432dd85714621e4b9383c82484
allenlr <allenlru55@gmail.com>
2023-09-27 19:14:26 -0500

    create oldPassword input in Account and adjust details' input alignments to sit flush again

--- client/src/Account.js
+++ client/src/Account.js
@@ -7,10 +7,12 @@ function Account(){
     
     const [newUsername, setNewUsername] = useState(currentUser?.user.username)
     const [newPassword, setNewPassword] = useState('')
+    const [oldPassword, setOldPassword] = useState('')
     const [newEmail, setNewEmail] = useState(currentUser?.user.email);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+    const [showOldPassword, setShowOldPassword] = useState(false);
 
     function handleEditUserSubmit(e){
         e.preventDefault()
@@ -31,7 +33,7 @@ function Account(){
                 <div>
                     <label>
                         New Password:
-                        <input style={{marginLeft:"8.7px"}} type={showPassword ? "text" : "password"} value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required />
+                        <input style={{marginLeft:"8px"}} type={showPassword ? "text" : "password"} value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required />
                     </label>
                     <button style={{fontSize: "10px", marginLeft: "5px"}} type="button" onClick={() => setShowPassword((prev) => !prev)}>
                         {showPassword ? "🚫👁️" : "👁️"}
@@ -39,8 +41,17 @@ function Account(){
                 </div>
                 <div>
                     <label>
+                        Old Password:
+                        <input style={{marginLeft:"14.6px"}} type={showOldPassword ? "text" : "password"} value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} required />
+                    </label>
+                    <button style={{fontSize: "10px", marginLeft: "5px"}} type="button" onClick={() => setShowOldPassword((prev) => !prev)}>
+                        {showOldPassword ? "🚫👁️" : "👁️"}
+                    </button>
+                </div>
+                <div>
+                    <label>
                         New Email:
-                        <input style={{marginLeft:"37px"}} type="text" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} required />
+                        <input style={{marginLeft:"36.6px"}} type="text" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} required />
                     </label>
                 </div>
                 <button id="save-changes-button" type="submit" disabled={loading}>
